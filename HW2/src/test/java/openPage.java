6d544cb1a68df71a31bb7f4d5ce217db06c0a3b4
Dzmitry <dmitry@MacBook-Pro-Dzmitry.local>
2023-02-21 01:05:10 +0300

    add cases

--- HW2/src/test/java/openPage.java
+++ HW2/src/test/java/openPage.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class OpenPage {
-}
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class openPage {
+
+    @Test
+    void firstTest(){ open("https://demoqa.com/automation-practice-form");
+       $("#firstName").setValue("Name");
+       $("#lastName").setValue("LastName");
+       $("#userEmail").setValue("User@name.ru");
+       $("#userNumber").setValue("123456789");
+       $("#currentAddress").setValue("Lenina Streets build 1");
+
+} }
